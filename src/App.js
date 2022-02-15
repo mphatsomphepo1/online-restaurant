204a0ff86cadbdfbad8e893a0a417a7e976c4386
MphatsoMphepo <bsc-inf-20-19@unima.ac.mw>
2022-02-15 09:42:52 +0200

    fixed a bug and added a logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import {} from "react-router-dom";
 
 function App() {
-    return ( <div className ="App" >
-    	<Navbar/>
+    return ( < div className = "App" >
+        <Navbar / >
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-import React from 'react'; 7.2K (gzipped: 2.9K)
+import React from 'react';
 import Logo from "../assets/logo.png";
 function Navbar(){
-	return {
+	return (
 		<div className="navbar">
 		<div className="leftSide">
 		<img src={Logo}/>
+		</div>
 		<div className="rightSide">
 		</div>
 		</div>
-	}
+	);
 }
 
 export default Navbar;
