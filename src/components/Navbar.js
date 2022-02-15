7637db77401464e419b5335838aa90e2287d9051
MphatsoMphepo <bsc-inf-20-19@unima.ac.mw>
2022-02-15 07:00:29 +0200

    changed the repository for easy tracking of the code because i've started using react

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'; 7.2K (gzipped: 2.9K)
+import Logo from "../assets/logo.png";
+function Navbar(){
+	return {
+		<div className="navbar">
+		<div className="leftSide">
+		<img src={Logo}/>
+		<div className="rightSide">
+		</div>
+		</div>
+	}
+}
+
+export default Navbar;
